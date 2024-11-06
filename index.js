506a4a718b72effc683158e58fdbdab55e8bc9a1
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-11-06 12:11:51 +0000

    added some basic code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello to codespaces");
+
+const app=require('./app');
+require('dotenv').config();
+
+//listen to a port
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is listening at port no: ${process.env.PORT}`);
+})
